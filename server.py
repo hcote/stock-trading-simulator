6b3744153ccbf483bf160f3fbe01e861fe16cbaf
Hunter Cote <hcote12@gmail.com>
2018-10-01 15:01:27 -0700

    updating server.py

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return render_template('index.html')
+@app.route("/", methods=['POST', 'GET'])
+def index():
+    if request.method=='POST':
+    ticker = request.form['ticker']
+    return render_template('index.html', ticker = ticker)
   
 if __name__ == "__main__":
-  app.run()
+    app.run()
 
