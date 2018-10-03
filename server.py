96b13178289a05ad16a674810c023eb10795e1bc
Hunter Cote <hcote12@gmail.com>
2018-10-03 15:49:30 -0700

    successfully connected MySQL into application

--- server.py
+++ server.py
@@ -1,12 +1,35 @@
 from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
-app = Flask(__name__)
+app = Flask (__name__)
 
-@app.route("/", methods=['POST', 'GET'])
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'stockTradingSimulator'
+
+mysql = MySQL(app)
+
+@app.route("/", methods=['GET'])
 def index():
-    if request.method=='POST':
-    ticker = request.form['ticker']
-    return render_template('index.html', ticker = ticker)
+    # ticker = request.form['ticker']
+    return render_template('index.html')
+  
+@app.route("/signup", methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        print(request)
+        # fetch form data
+        user_datails = request.form
+        username = user_datails['username']
+        password = user_datails['password']
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO users(username, password) VALUES(%s, %s)', (username, password))
+        mysql.connection.commit()
+        cur.close()
+        return 'success'
+    return render_template('signup.html')
   
 if __name__ == "__main__":
     app.run()
